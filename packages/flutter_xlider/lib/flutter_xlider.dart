4556c9bbb99f416e0c6d53769503961eadd96fa8
joegsuero <josedaniel.garcia@datazucar.cu>
2024-12-03 09:31:12 -0500

    Fix in iridium error from flutter_xlider

---
+++ packages/flutter_xlider/lib/flutter_xlider.dart
@@ -0,0 +1,65 @@
+/// A material design slider and range slider with horizontal and vertical axis, rtl support and lots of options and customizations for flutter
+
+/*
+* *
+* * Written by Ali Azmoude <ali.azmoude@gmail.com>
+* *
+* *
+* *
+* * When I wrote this, only God and I understood what I was doing.
+* * Now, God only knows "Karl Weierstrass"
+* */
+
+library flutter_slider;
+
+// export enum
+export './src/model/enum/flutter_slider_hatch_mark_alignment.dart';
+export './src/model/enum/flutter_slider_tooltip_direction.dart';
+
+// export model
+export './src/model/flutter_slider_fixed_value.dart';
+export './src/model/flutter_slider_handler.dart';
+export './src/model/flutter_slider_handler_animation.dart';
+export './src/model/flutter_slider_hatch_mark.dart';
+export './src/model/flutter_slider_hatch_mark_label.dart';
+export './src/model/flutter_slider_ignore_steps.dart';
+export './src/model/flutter_slider_range_step.dart';
+export './src/model/flutter_slider_sized_box.dart';
+export './src/model/flutter_slider_step.dart';
+export './src/model/flutter_slider_tooltip.dart';
+export './src/model/flutter_slider_tooltip_box.dart';
+export './src/model/flutter_slider_tooltip_position_offset.dart';
+export './src/model/flutter_slider_trackbar.dart';
+
+// export view
+export './src/view/flutter_slider_widget.dart';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
